beef0430a806b77685c2f19c7e38fbb7d0ac6778
BravenMo <Varmamohit53@gmail.com>
2025-05-22 01:05:32 +0530

    Updated changes to home component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -8,6 +8,40 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+
+  private targetElement: HTMLElement | null = null;
+  private isAnimated: boolean = false;
+
+  constructor(private el: ElementRef) {}
+
+  ngOnInit(): void {
+    this.targetElement = this.el.nativeElement.querySelector('#textContainer');
+    this.checkVisibility();  // Check initially in case it is already in view
+  }
+
+  @HostListener('window:scroll', [])
+  onScroll(): void {
+    this.checkVisibility();
+  }
+
+  private checkVisibility(): void {
+    if (this.targetElement && !this.isAnimated) {
+      const rect = this.targetElement.getBoundingClientRect();
+      const windowHeight = window.innerHeight || document.documentElement.clientHeight;
+
+      if (rect.top <= windowHeight * 0.8) {  // 80% of the viewport height
+        this.targetElement.classList.add('animate-slide-in');
+        this.targetElement.classList.remove('hidden-slide');
+        this.isAnimated = true;  // Prevents multiple triggers
+      }
+    }
+  }
+
+  ngOnDestroy(): void {
+    // Clean up listeners when component is destroyed
+    window.removeEventListener('scroll', this.onScroll);
+  }
+
 
   hi() {
     console.log("Hi");
